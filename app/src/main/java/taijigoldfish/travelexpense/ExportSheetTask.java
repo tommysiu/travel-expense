5c555d7e3ec341e74f094ee21e2a5b9e852316bb
Tommy Siu <yhsiu93@gmail.com>
2016-11-17 22:05:16 +0800

    Refactor google sheet export function

--- app/src/main/java/taijigoldfish/travelexpense/ExportSheetTask.java
+++ app/src/main/java/taijigoldfish/travelexpense/ExportSheetTask.java
@@ -16,6 +16,7 @@ import com.google.api.services.sheets.v4.model.ExtendedValue;
 import com.google.api.services.sheets.v4.model.GridData;
 import com.google.api.services.sheets.v4.model.RowData;
 import com.google.api.services.sheets.v4.model.Sheet;
+import com.google.api.services.sheets.v4.model.SheetProperties;
 import com.google.api.services.sheets.v4.model.Spreadsheet;
 import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
 import com.google.api.services.sheets.v4.model.TextFormat;
@@ -24,6 +25,7 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -35,6 +37,8 @@ public class ExportSheetTask extends AsyncTask<Void, Void, Void> {
     private MainActivity activity;
     private Sheets service = null;
     private Exception lastError = null;
+
+    private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
 
     ExportSheetTask(MainActivity activity, GoogleAccountCredential credential) {
         this.activity = activity;
@@ -48,7 +52,7 @@ public class ExportSheetTask extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... params) {
         try {
-            createData();
+            createSheet();
         } catch (Exception e) {
             this.lastError = e;
             cancel(true);
@@ -56,56 +60,55 @@ public class ExportSheetTask extends AsyncTask<Void, Void, Void> {
         return null;
     }
 
-    private List<String> createData() throws IOException {
+    private void createSheet() throws IOException {
         Trip trip = this.activity.getCurrentTrip();
+
+        Sheet general = new Sheet();
+        general.setProperties(new SheetProperties().setTitle("General"));
+        general.setData(createGeneralInfo(trip));
+
+        Sheet expense = new Sheet();
+        expense.setProperties(new SheetProperties().setTitle("Expense"));
+        expense.setData(createExpenseData(trip));
+
         Spreadsheet spreadsheet = new Spreadsheet();
-        SpreadsheetProperties properties = new SpreadsheetProperties();
-        Sheet sheet = new Sheet();
-        sheet.setData(createSheetData(trip));
-
-        properties.setTitle(Utils.getTripTitle(this.activity, trip));
-        spreadsheet.setProperties(properties);
-
-        spreadsheet.setSheets(Collections.singletonList(sheet));
+        spreadsheet.setProperties(new SpreadsheetProperties().setTitle(generateFileName(trip)));
+        spreadsheet.setSheets(Arrays.asList(general, expense));
 
         Sheets.Spreadsheets.Create create = this.service.spreadsheets().create(spreadsheet);
         create.execute();
-
-        return null;
     }
 
-    private List<GridData> createSheetData(Trip trip) {
-        List<GridData> gridDataList = new ArrayList<>();
-
-        RowData header = new RowData();
-        header.setValues(createHeader());
-        GridData data = new GridData();
-        List<RowData> rows = new ArrayList<>();
-
-        rows.addAll(createSummary(trip));
-
-        rows.add(header);
-        for (Integer day : trip.getItemMap().keySet()) {
-            rows.addAll(createRows(trip, day));
-        }
-        data.setRowData(rows);
-        gridDataList.add(data);
-
-        return gridDataList;
+    private String generateFileName(Trip trip) {
+        return this.dateFormat.format(trip.getStartDate()) + " " + trip.getDestination() + " Expense";
     }
 
-    private List<RowData> createSummary(Trip trip) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+    private List<GridData> createGeneralInfo(Trip trip) {
+        GridData grid = new GridData();
         List<RowData> list = new ArrayList<>();
 
         list.add(createRow("Destination", trip.getDestination()));
-        list.add(createRow("Start Date", dateFormat.format(trip.getStartDate())));
-        list.add(createRow("End Date", dateFormat.format(trip.getEndDate())));
+        list.add(createRow("Start Date", this.dateFormat.format(trip.getStartDate())));
+        list.add(createRow("End Date", this.dateFormat.format(trip.getEndDate())));
         list.add(createRow("Currency", trip.getCurrency()));
         list.add(createRow("Total Cash", Float.toString(trip.getTotalCash())));
-        list.add(new RowData());
 
-        return list;
+        grid.setRowData(list);
+        return Collections.singletonList(grid);
+    }
+
+    private List<GridData> createExpenseData(Trip trip) {
+        RowData header = new RowData();
+        header.setValues(createExpenseHeader());
+        GridData grid = new GridData();
+        List<RowData> rows = new ArrayList<>();
+
+        rows.add(header);
+        for (Integer day : trip.getItemMap().keySet()) {
+            rows.addAll(createDayExpenses(trip, day));
+        }
+        grid.setRowData(rows);
+        return Collections.singletonList(grid);
     }
 
     private RowData createRow(String header, String... params) {
@@ -125,7 +128,7 @@ public class ExportSheetTask extends AsyncTask<Void, Void, Void> {
         return row;
     }
 
-    private List<CellData> createHeader() {
+    private List<CellData> createExpenseHeader() {
         List<CellData> list = new ArrayList<>();
         String[] headers = {"Day", "Type", "Desc", "Pay Type", "Amount"};
         for (String h : headers) {
@@ -138,7 +141,7 @@ public class ExportSheetTask extends AsyncTask<Void, Void, Void> {
         return list;
     }
 
-    private List<RowData> createRows(Trip trip, Integer day) {
+    private List<RowData> createDayExpenses(Trip trip, Integer day) {
         List<Item> items = trip.getItemMap().get(day);
         List<RowData> rows = new ArrayList<>();
         for (Item item : items) {
@@ -158,18 +161,16 @@ public class ExportSheetTask extends AsyncTask<Void, Void, Void> {
         return rows;
     }
 
-    private CellData createCell(String data) {
-        return new CellData()
-                .setUserEnteredValue(new ExtendedValue().setStringValue(data))
-                .setUserEnteredFormat(new CellFormat()
-                        .setHorizontalAlignment("RIGHT"));
-    }
-
-    private CellData createCell(Number data) {
-        return new CellData()
-                .setUserEnteredValue(new ExtendedValue().setNumberValue(data.doubleValue()))
-                .setUserEnteredFormat(new CellFormat()
-                        .setHorizontalAlignment("RIGHT"));
+    private CellData createCell(Object data) {
+        CellData cellData = new CellData()
+                .setUserEnteredFormat(new CellFormat().setHorizontalAlignment("RIGHT"));
+        if (data instanceof Double) {
+            Double val = (Double) data;
+            cellData.setUserEnteredValue(new ExtendedValue().setNumberValue(val));
+        } else {
+            cellData.setUserEnteredValue(new ExtendedValue().setStringValue(data.toString()));
+        }
+        return cellData;
     }
 
     @Override
